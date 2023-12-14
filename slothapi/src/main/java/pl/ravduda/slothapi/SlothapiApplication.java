d1d10e24b2b18d70c6999b060ff554d87abbecbf
ravduda <ravduda@gmail.com>
2023-12-14 12:15:13 +0100

    Initial commit

---
+++ slothapi/src/main/java/pl/ravduda/slothapi/SlothapiApplication.java
@@ -0,0 +1,13 @@
+package pl.ravduda.slothapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlothapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlothapiApplication.class, args);
+	}
+
+}
---
+++ slothapi/src/test/java/pl/ravduda/slothapi/SlothapiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.ravduda.slothapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlothapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
